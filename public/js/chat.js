ec76122b92225cea4c4e5e17b72e7feb645a50c6
marx99 <maxingzhe99@126.com>
2016-10-26 08:48:30 +0800

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,7 +5,7 @@ $(function () {
     // 默认链接到渲染页面的服务器
     var socket = io();
     function scrollToBottom () {
-        $('#chat-area').scrollTop($('#chat-area')[0].scrollHeight);
+        //$('#chat-area').scrollTop($('#chat-area')[0].scrollHeight);
     };
     function getColor (proClose,newPrice) {
 		if (newPrice==0)
@@ -24,6 +24,37 @@ $(function () {
 			return "gray";
 		}
     };
+
+	var getnewsina = function(){
+		//alert("http://image.sinajs.cn/newchart/min/n/" + getSzSh($('#hidstockno').val()) + ".gif?t=" + new Date());
+		$("#imgstock").attr("src","http://image.sinajs.cn/newchart/min/n/" + getSzSh($('#hidstockno').val()) + ".gif?t=" + new Date());
+		//$("#btntest").val(new Date());
+	};
+
+	//定时刷新最新图片
+	setInterval(getnewsina,100000);
+	
+	$("#danmu").danmu({
+		height: 300,  //弹幕区高度
+		width: 540,   //弹幕区宽度
+		zindex :100,   //弹幕区域z-index属性
+		speed:20000,      //滚动弹幕的默认速度，这是数值值得是弹幕滚过每672像素所需要的时间（毫秒）
+		sumTime:195535,   //弹幕流的总时间
+		danmuLoop:false,   //是否循环播放弹幕
+		defaultFontColor:"#FFFFFF",   //弹幕的默认颜色
+		fontSizeSmall:16,     //小弹幕的字号大小
+		FontSizeBig:24,       //大弹幕的字号大小
+		opacity:"0.9",          //默认弹幕透明度
+		topBottonDanmuTime:6000,   // 顶部底部弹幕持续时间（毫秒）
+		SubtitleProtection:false,     //是否字幕保护
+		positionOptimize:false,         //是否位置优化，位置优化是指像AB站那样弹幕主要漂浮于区域上半部分
+
+		maxCountInScreen: 40,   //屏幕上的最大的显示弹幕数目,弹幕数量过多时,优先加载最新的。
+		maxCountPerSec: 10      //每分秒钟最多的弹幕数目,弹幕数量过多时,优先加载最新的。
+	});
+	
+	$('#danmu').danmu('danmuResume');
+
 	$("#stockno").keydown(function (e) {
  			//$('.error').text(e.which);
        if (e.which === 13) {
@@ -55,7 +86,7 @@ $(function () {
 			return false;
 		}
 
-		window.location.href='/room/' + strvalue;
+		window.location.href='/' + strvalue;
 	};
 
     socket.on('connect', function () {
@@ -76,6 +107,8 @@ $(function () {
         $('.time').text(data.time);
         $('.roomid').text(stocklist[data.roomid] + " (" + data.roomid + ")");
 		var jsondata = jQuery.parseJSON(data.content);
+	$("img").attr("src","http://image.sinajs.cn/newchart/min/n/" + getSzSh(data.roomid) + ".gif");
+        $('#hidstockno').val(data.roomid);
         //$('.content').text("");
 		//现价
         //$('.match').text(jsondata.data.match);
